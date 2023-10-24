5711027e78449c78a5741c8d8d7e43bda083437e
MakarosVar <49597451+MakarosVar@users.noreply.github.com>
2023-10-24 23:53:33 +0300

    update, delete income

--- server/src/controllers/income/incomeCtrl.js
+++ server/src/controllers/income/incomeCtrl.js
@@ -1,36 +1,79 @@
-import expressAsyncHandler from 'express-async-handler';
-import Income from '../../models/Income.js';
+import expressAsyncHandler from "express-async-handler";
+import Income from "../../models/Income.js";
 
-//create new income 
+//create new income
 
 const createIncomeCtrl = expressAsyncHandler(async (req, res, next) => {
-    const{title, description, amount, type} = req?.body;
-    try {
-        const newIncome = new Income({
-            title,
-            description,
-            amount,
-            type,
-            user: req.user._id
-        });
-        await newIncome.save();
-        res.status(201).json({message: 'Income created successfully'});
-    } catch (error) {
-        next(error);
-    }
-
+  const { title, description, amount, type } = req?.body;
+  try {
+    const newIncome = new Income({
+      title,
+      description,
+      amount,
+      type,
+      user: req.user._id,
+    });
+    await newIncome.save();
+    res.status(201).json({ message: "Income created successfully" });
+  } catch (error) {
+    next(error);
+  }
 });
 
-//fetch all incomes 
+//fetch all incomes
 
 const fetchAllIncomesCtrl = expressAsyncHandler(async (req, res, next) => {
+  try {
+    const incomes = await Income.find({});
+    res.status(200).json(incomes);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//fetch one income
+
+const fetchOneIncomeCtrl = expressAsyncHandler(async (req, res, next) => {
+  try {
+    const income = await Income.findById(req.params.id);
+    res.status(200).json(income);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//update income
+
+const updateIncomeCtrl = expressAsyncHandler(async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { title, description, amount, type } = req.body;
+    const income = await Income.findByIdAndUpdate(
+      id,
+      {
+        title,
+        description,
+        amount,
+        type,
+      },
+      { new: true }
+    );
+    res.status(200).json(income);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//delete income
+
+const deleteIncomeCtrl = expressAsyncHandler(async (req, res, next) => {
     try {
-        const incomes = await Income.find({});
-        res.status(200).json(incomes);
+        const { id } = req.params;
+        await Income.findByIdAndDelete(id);
+        res.status(200).json({ message: "Income deleted successfully" });
     } catch (error) {
         next(error);
     }
 });
 
-export {createIncomeCtrl, fetchAllIncomesCtrl};
-
+export { createIncomeCtrl, fetchAllIncomesCtrl, fetchOneIncomeCtrl, updateIncomeCtrl, deleteIncomeCtrl };
--- server/src/routes/income/IncomeRoute.js
+++ server/src/routes/income/IncomeRoute.js
@@ -1,8 +1,17 @@
 import Express from "express";
-import  {createIncomeCtrl,fetchAllIncomesCtrl}  from "../../controllers/income/incomeCtrl.js";
+import {
+  createIncomeCtrl,
+  fetchAllIncomesCtrl,
+  fetchOneIncomeCtrl,
+  updateIncomeCtrl,
+  deleteIncomeCtrl
+} from "../../controllers/income/incomeCtrl.js";
 
 const incomeRoute = Express.Router();
 incomeRoute.post("/", createIncomeCtrl);
 incomeRoute.get("/", fetchAllIncomesCtrl);
+incomeRoute.get("/:id", fetchOneIncomeCtrl);
+incomeRoute.put("/:id", updateIncomeCtrl);
+incomeRoute.delete("/:id", deleteIncomeCtrl);
 
 export default incomeRoute;
