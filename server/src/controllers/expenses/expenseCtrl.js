3a5bad466c0437eb569226ab0ca092e34634dba6
MakarosVar <49597451+MakarosVar@users.noreply.github.com>
2023-10-25 22:25:07 +0300

    add CRUD for expenses

---
+++ server/src/controllers/expenses/expenseCtrl.js
@@ -0,0 +1,99 @@
+import expressAsyncHandler from 'express-async-handler';
+import Expense from '../../models/expense';
+
+//create new income
+
+const createExpenseCtrl = expressAsyncHandler(
+  async (req, res, next) => {
+    const { title, description, amount, type } = req?.body;
+    try {
+      const newExpense = new Expense({
+        title,
+        description,
+        amount,
+        type,
+        user: req.user._id,
+      });
+      await newExpense.save();
+      res
+        .status(201)
+        .json({ message: 'Expense created successfully' });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//fetch all Expenses
+
+const fetchAllExpensesCtrl = expressAsyncHandler(
+  async (req, res, next) => {
+    try {
+      const expenses = await Expense.find({});
+      res.status(200).json(expenses);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//fetch one Expense
+
+const fetchOneExpenseCtrl = expressAsyncHandler(
+  async (req, res, next) => {
+    try {
+      const expense = await Expense.findById(req.params.id);
+      res.status(200).json(expense);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//update expenses
+
+const updateExpenseCtrl = expressAsyncHandler(
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { title, description, amount, type } = req.body;
+      const expense = await Expense.findByIdAndUpdate(
+        id,
+        {
+          title,
+          description,
+          amount,
+          type,
+        },
+        { new: true }
+      );
+      res.status(200).json(expense);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+//delete expense
+
+const deleteExpenseCtrl = expressAsyncHandler(
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await Expense.findByIdAndDelete(id);
+      res
+        .status(200)
+        .json({ message: 'Expense deleted successfully' });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+export {
+  createExpenseCtrl,
+  fetchAllExpensesCtrl,
+  fetchOneExpenseCtrl,
+  updateExpenseCtrl,
+  deleteExpenseCtrl,
+};
---
+++ server/src/routes/expenses/ExpenseRoute.js
@@ -0,0 +1,17 @@
+import Express from 'express';
+import {
+  createExpenseCtrl,
+  fetchAllExpensesCtrl,
+  fetchOneExpenseCtrl,
+  updateExpenseCtrl,
+  deleteExpenseCtrl,
+} from '../../controllers/expense/expenseCtrl.js';
+
+const expenseRoute = Express.Router();
+expenseRoute.post('/', createExpenseCtrl);
+expenseRoute.get('/', fetchAllExpensesCtrl);
+expenseRoute.get('/:id', fetchOneExpenseCtrl);
+expenseRoute.put('/:id', updateExpenseCtrl);
+expenseRoute.delete('/:id', deleteExpenseCtrl);
+
+export default expenseRoute;
