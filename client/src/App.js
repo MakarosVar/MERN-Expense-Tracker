34880dd92116c1f818457affa19266833bc072b4
MakarosVar <christomvarv@yahoo.com>
2023-10-29 21:51:36 +0200

    fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,13 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Profile } from "./pages/Profile";
-import {Signup} from "./pages/Signup";
-import { Signin } from "./pages/Signin";
+import  Home  from "./pages/Home";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/profile" element={<Profile/>} />
+        <Route path="/" Component={Home} />
       </Routes>
     </BrowserRouter>
   );
