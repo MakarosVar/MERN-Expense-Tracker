5bf2d64de25c3207b9c0e6c93e664d5e1996108f
MakarosVar <christomvarv@yahoo.com>
2023-10-31 22:45:31 +0200

    add login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
+import Login from './Pages/Users/Login';
 import './App.css';
 function App() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" Component={Home} />
+        <Route exact path="/login" Component={Login} />
       </Routes>
     </Router>
   );
