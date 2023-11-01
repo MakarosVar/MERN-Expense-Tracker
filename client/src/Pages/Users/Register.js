d036d56a985a955228b8efe9a94b8aeda11be8eb
MakarosVar <christomvarv@yahoo.com>
2023-11-01 15:51:11 +0200

    register page

--- client/src/Pages/Users/Register.js
+++ client/src/Pages/Users/Register.js
@@ -1,11 +1,9 @@
 import React from "react";
 
-const Login = () => {
+const Register = () => {
+  //Redirect
   return (
-    <section
-      style={{ height: "100vh" }}
-      className="position-relative py-5 overflow-hidden bg-warning"
-    >
+    <section className="position-relative py-5 overflow-hidden vh-100">
       <div className="d-none d-md-block position-absolute top-0 start-0 bg-dark w-75 h-100"></div>
       <div className="d-md-none position-absolute top-0 start-0 bg-primary w-100 h-100"></div>
       <div className="container position-relative mx-auto">
@@ -13,33 +11,41 @@ const Login = () => {
           <div className="col-12 col-lg-5 mb-5">
             <div>
               <h2 className="display-5 fw-bold mb-4 text-white">
-                Keep Track of what you are spending
+                Keep Track of your income and expenses flow
               </h2>
-              <hr className="text-warning w-100" />
             </div>
           </div>
           <div className="col-12 col-lg-5 ms-auto">
             <div className="p-5 bg-light rounded text-center">
-              <span className="text-muted">Sign In</span>
-              <h3 className="fw-bold mb-5">Login to your account</h3>
               <form>
+                <span className="text-muted">New User</span>
+                <h3 className="fw-bold mb-5">Register</h3>
+                <input
+                  className="form-control mb-2"
+                  type="text"
+                  placeholder="First Name"
+                />
+                <input
+                  className="form-control mb-2"
+                  type="TEXT"
+                  placeholder="Last Name"
+                />
                 <input
                   className="form-control mb-2"
                   type="email"
-                  placeholder="Email-address"
+                  placeholder="Email"
                 />
                 <input
                   className="form-control mb-2"
                   type="password"
                   placeholder="Password"
                 />
-                <div>
                   <button
                     type="submit"
-                    className="btn btn-primary py-2 w-100 mb-4">
-                    Login
+                    className="btn btn-primary py-2 w-100 mb-4"
+                  >
+                    Register
                   </button>
-                </div>
               </form>
             </div>
           </div>
@@ -49,4 +55,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
