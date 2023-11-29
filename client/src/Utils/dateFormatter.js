e52c1f82167cd4731bf6c77d0d9ff61def843db2
MakarosVar <christomvarv@yahoo.com>
2023-11-29 23:14:53 +0200

    Create dateFormatter.js

---
+++ client/src/Utils/dateFormatter.js
@@ -0,0 +1,7 @@
+const dateFormatter= (date) => {
+    const newDate = new Date(date);
+    const year = new Intl.DateTimeFormat("en", { year: "numeric" }).format(newDate);
+    const month = new Intl.DateTimeFormat("en", { month: "short" }).format(newDate);
+    const day = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(newDate);
+    return `${day}-${month}-${year}`;
+};
